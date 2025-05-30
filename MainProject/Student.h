571254a9f4c3978aa20f3aa97019598114d14a56
ivanvikvik <ivanvikvik@gmail.com>
2025-05-30 19:44:17 +0300

    Add constructors and destructor into Student class описание коснтрукторов и деструктора класса

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -24,6 +24,57 @@ public:
 		mark = 4;
 	}
 
+	// constructor with arguments (конструкторы с параметрами)
+	Student(string name, string sname) {
+		//cout << "constructor with arguments" << endl;
+		firstname = name;
+		surname = sname;
+		age = 0;
+		_class = 0;
+		gender = 'm';
+		alive = false;
+		mark = 4;
+	}
+
+	Student(string name, string sname, int a) {
+		//cout << "constructor with arguments" << endl;
+		firstname = name;
+		surname = sname;
+		age = a;
+		_class = 0;
+		gender = 'm';
+		alive = false;
+		mark = 4;
+	}
+
+	// canonical constructor (канонический конструктор)
+	Student(string name, string sname, int a, int cl, char g, bool al, double m) {
+		//cout << "canonical constructor" << endl;
+		firstname = name;
+		surname = sname;
+		age = a;
+		_class = cl;
+		gender = g;
+		alive = al;
+		mark = m;
+	}
+
+	// copy-constructor (конструктор-копирования)
+	Student(const Student& student) {
+		//cout << "canonical constructor" << endl;
+		firstname = student.firstname;
+		surname = student.surname;
+		age = student.age;
+		_class = student._class;
+		gender = student.gender;
+		alive = student.alive;
+		mark = student.mark;
+	}
+
+	~Student() {
+		cout << "destructor" << endl;
+	}
+
 	string toString() {
 		string s = firstname;
 		s += " " + surname;
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,11 +1,16 @@
 #include "Student.h"
 
 int main() {
-	Student st1, st2, st3;
+	Student st1;
+	Student st2("Alex", "Chichikov");
+	Student st3("Ivan", "Ivanov", 15);
+	Student st4("Olya", "Sidorova", 14, 8, 'f', true, 9);
+	Student st5(st4);
 
 	cout << st1.toString() << endl;
 	cout << st2.toString() << endl;
 	cout << st3.toString() << endl;
+	cout << st4.toString() << endl;
 
 	return 0;
 }
